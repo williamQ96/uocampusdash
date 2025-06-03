2e177c6df1f417347bb98e61d0f746107c01c8c0
Millie-8911 <128894092+Millie-8911@users.noreply.github.com>
2025-06-03 03:25:15 -0700

    Improve player's movement + Increase speed

--- uocampusdash/Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/First Person Player/PlayerMovement.cs
+++ uocampusdash/Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/First Person Player/PlayerMovement.cs
@@ -1,37 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace SojaExiles
-
 {
     public class PlayerMovement : MonoBehaviour
     {
-
         public CharacterController controller;
+        public Transform groundCheck;         // Check if player is on the ground
+        public LayerMask groundMask;        
 
         public float speed = 5f;
-        public float gravity = -15f;
+        public float gravity = -9.81f;
+        public float groundDistance = 0.4f;
 
-        Vector3 velocity;
+        private Vector3 velocity;
+        private bool isGrounded;
 
-        bool isGrounded;
-
-        // Update is called once per frame
         void Update()
         {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f; // Prevent from accumulate jumping or gravity
+            }
+
+            // Player Input
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
-
             Vector3 move = transform.right * x + transform.forward * z;
 
+            // Move Horizontally
             controller.Move(move * speed * Time.deltaTime);
 
+            // Apply Gravity
             velocity.y += gravity * Time.deltaTime;
-
             controller.Move(velocity * Time.deltaTime);
-
         }
     }
 }
